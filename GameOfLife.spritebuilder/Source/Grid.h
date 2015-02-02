d6049a5260ac6b74ebe5c25c3488e26cc8e48ed1
Issara Pong Paoluengtong <pp.issara@yahoo.com>
2015-02-02 18:53:51 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Source/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Issara Paoluengtong on 2/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
