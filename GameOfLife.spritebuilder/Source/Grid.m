4c048953385c698436238e6d7f8ddadff9f2cf25
Issara Pong Paoluengtong <pp.issara@yahoo.com>
2015-02-02 19:38:35 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -54,8 +54,7 @@ static const int GRID_COLUMNS = 10;
             //this is shorthand to access an array inside an array
             _gridArray[i][j] = creature;
             
-            //make creatures visible to test this method, remove this once we...
-            creature.isAlive = YES;
+          
             
             x+=_cellWidth;
         }
@@ -63,5 +62,28 @@ static const int GRID_COLUMNS = 10;
         y += _cellHeight;
     }
 }
+
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event{
+    //get the x,y coordinates of the touch
+    CGPoint touchLocation = [touch locationInNode:self];
+    
+    //get the creature at that location
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    
+    //invert it's state - kill it if it's alive, bring it to life if it's...
+    creature.isAlive = !creature.isAlive;
+}
+
+-(Creature *)creatureForTouchPosition:(CGPoint)touchPosition{
+    //get the row and column that was touched, return the Creature inside the corresponding cell
+    
+    int row = touchPosition.y/_cellHeight;
+    int column = touchPosition.x / _cellWidth;
+    
+    return _gridArray[row][column];
+}
+
+
+
 
 @end
